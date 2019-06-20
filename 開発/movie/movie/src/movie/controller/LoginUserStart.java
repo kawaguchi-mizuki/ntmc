102b03b64d80f92616640a99ccdaea766864a371
mizuki <1701129@st.asojuku.ac.jp>
2019-06-20 09:24:02 +0900

    開発用　サンプル

---
+++ 開発/movie/movie/src/movie/controller/LoginUserStart.java
@@ -0,0 +1,23 @@
+package movie.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/login")
+public class LoginUserStart extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse respons) throws ServletException, IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		respons.setContentType("text/css");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login_user.jsp");
+		dispatcher.forward(request, respons);
+	}
+
+
+}
