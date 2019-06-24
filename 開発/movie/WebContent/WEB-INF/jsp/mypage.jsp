ec09a0ebceeffa7c6c2863bc381df338603af69d
mizuki <1701129@st.asojuku.ac.jp>
2019-06-24 10:59:06 +0900

    最新版　です

---
+++ 開発/movie/WebContent/WEB-INF/jsp/mypage.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>マイページ</title>
+<link href="css/mypage.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<table align="center">
+		<tr>
+			<th><a href="unsub" class="btn-square">退会続き</a></th>
+			<th><a href="updateuserstart" class="btn-square">会員情報変更</a></th>
+		</tr>
+		<tr>
+			<td><a href="appointcheck" class="btn-square">予約確認</a></td>
+		</tr>
+	</table>
+
+</body>
+</html>
---
+++ 開発/movie/src/movie/controller/UpdateUserStart.java
@@ -0,0 +1,20 @@
+package movie.controller;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/updateuserstart")
+public class UpdateUserStart extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse respons) throws ServletException, IOException {
+		// TOD 自動生成されたメソッド・スタブ
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/update_user.jsp");
+		dispatcher.forward(request, respons);
+
+	}
+
+}
