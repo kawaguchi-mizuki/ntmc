dd3831e69630a54d779e6f047fff8b611261cb02
kihiro18n36 <50507982+kihiro18n36@users.noreply.github.com>
2019-07-03 10:56:50 +0900

    Add files via upload

---
+++ 開発/src/model/UserModel.java
@@ -0,0 +1,180 @@
+package model;
+
+
+import beans.AdminInfoBeans;
+import beans.UserInfoBeans;
+import model.dao.UserDao;
+
+
+public class UserModel {
+
+
+
+	//ログイン処理
+
+	public UserInfoBeans login(String mail,String password) {
+
+		UserInfoBeans login = null;
+
+
+		UserDao userDao = new UserDao();
+
+		try {
+
+			userDao.connect();
+
+			login = userDao.getFromUser(mail,password);
+
+		} catch(Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			if(userDao!=null) {
+
+				userDao.close();
+
+			}
+
+		}
+
+		return login;
+
+
+
+	}
+
+
+	//会員登録
+
+	public void createUser(String mail,String name,String sex, String
+			birthday,String password) {
+
+
+		UserDao userDao = new UserDao();
+
+
+		try {
+
+
+			userDao.connect();
+
+
+			userDao.createUser(mail,name,sex,birthday,password);
+
+
+		}catch(Exception e) {
+
+
+			e.printStackTrace();
+
+		}
+
+		finally {
+
+
+			if(userDao != null) {
+
+				userDao.close();
+
+			}
+
+		}
+
+	}
+
+
+	//会員情報変更
+
+	public void  changeInfoUser(String mail,String name,String password) {
+
+
+		UserDao userDao = new UserDao();
+
+
+		try {
+
+
+			userDao.connect();
+
+
+			userDao.changeInfoUser(mail,name,password);
+
+
+		}catch(Exception e) {
+
+
+			e.printStackTrace();
+
+		}
+		finally {
+
+
+			if(userDao != null) {
+
+				userDao.close();
+
+			}
+
+		}
+
+
+
+	}
+
+	//管理人ログイン処理 7/03
+
+	public AdminInfoBeans Adminlogin(String mail,String password) {
+
+		AdminInfoBeans adminLogin = null;
+
+
+		UserDao userDao = new UserDao();
+
+		try {
+
+			userDao.connect();
+
+		adminLogin = userDao.getFromAdmin(mail,password);
+
+		} catch(Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			if(userDao!=null) {
+
+				userDao.close();
+
+			}
+
+		}
+
+		return adminLogin;
+
+
+
+	}
+
+	public void unsubFromUser(int userId) {
+		UserDao userDao = new UserDao();
+		try{
+			///////////////////////////////////
+			//DBの接続
+			userDao.connect();
+
+			userDao.deleteUser(userId);
+		}catch(Exception e) {
+			//エラー発生した場合にコンソールにログを出力する
+			e.printStackTrace();
+		}
+		finally {
+			//接続（コネクション）を閉じる
+			if(userDao!=null) {
+				userDao.close();
+			}
+		}
+	}
+}
