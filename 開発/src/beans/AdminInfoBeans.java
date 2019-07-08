4d2d932a34a02b26bae3aceaed1f7988f0208835
kihiro18n36 <50507982+kihiro18n36@users.noreply.github.com>
2019-07-08 09:53:07 +0900

    修正しました

--- 開発/src/beans/AdminInfoBeans.java
+++ 開発/src/beans/AdminInfoBeans.java
@@ -3,54 +3,36 @@
 package beans;
 
 import java.io.Serializable;
-import java.sql.Date;
 
 public class AdminInfoBeans implements Serializable {
+
 	private int id;
+
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	private String mail;
+
 	private String name;
-	private String sex;
+
 	private String password;
-	private Date birthday;
-	public String getMail() {
-		return mail;
-	}
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
+
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getSex() {
-		return sex;
-	}
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
+
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Date getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-
-
-
 
 }
